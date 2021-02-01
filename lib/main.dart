7b6da8a89dbdd3b94757b0113cc5a26abf45f436
johnny <>
2021-02-01 18:13:23 +0800

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,14 @@ class WeatherDemoApp extends StatelessWidget {
   }
 }
 
+// 都是用同一個場景,所以只產生一個就好
 ANArea rootNode;
 
 ImageMap _images;
 
 int currentBtnId = 0;
+
+bool assetsLoaded = false;
 
 class FirstPage extends StatefulWidget {
   @override
@@ -30,6 +33,32 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
+  Future<Null> _loadAssets(AssetBundle bundle) async {
+    // Load images using an ImageMap
+    _images = new ImageMap(bundle);
+    // 使用load 方法 (非同步) 去取得圖片
+    await _images.load(<String>[
+      'assets/diamond.png',
+      'assets/light_spot.png',
+    ]);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    AssetBundle bundle = rootBundle;
+    _loadAssets(bundle).then(
+      (value) {
+        setState(() {
+          assetsLoaded = true;
+          rootNode = new ANArea();
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +73,7 @@ class _FirstPageState extends State<FirstPage> {
                 buttonId: 0,
                 onPressed: () {
                   setState(() {
+                    rootNode.active = true;
                     currentBtnId = 0;
                   });
                 }),
@@ -51,6 +81,7 @@ class _FirstPageState extends State<FirstPage> {
                 buttonId: 1,
                 onPressed: () {
                   setState(() {
+                    rootNode.active = true;
                     currentBtnId = 1;
                   });
                 }),
@@ -58,6 +89,7 @@ class _FirstPageState extends State<FirstPage> {
                 buttonId: 2,
                 onPressed: () {
                   setState(() {
+                    rootNode.active = true;
                     currentBtnId = 2;
                   });
                 })
@@ -79,48 +111,30 @@ class AnimatedBGButton extends StatefulWidget {
 }
 
 class _AnimatedBGButtonState extends State<AnimatedBGButton> {
-  bool assetsLoaded = false;
-  Future<Null> _loadAssets(AssetBundle bundle) async {
-    // Load images using an ImageMap
-    _images = new ImageMap(bundle);
-    // 使用load 方法 (非同步) 去取得圖片
-    await _images.load(<String>[
-      'assets/diamond.png',
-      'assets/light_spot.png',
-    ]);
-  }
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
-    AssetBundle bundle = rootBundle;
-    _loadAssets(bundle).then(
-      (value) {
-        setState(() {
-          assetsLoaded = true;
-          rootNode = new ANArea(btnId: widget.buttonId);
-        });
-      },
-    );
   }
 
+  // 目前按鈕的比例是用2(寬):1(高)做的,如果不照這個比例也可以跑,只是比較醜
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: widget.onPressed,
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10.0),
-        width: 120,
-        height: 60,
+        width: 150,
+        height: 75,
         decoration: BoxDecoration(
-            color: Colors.orangeAccent,
+            color: currentBtnId == widget.buttonId
+                ? Colors.orangeAccent
+                : Colors.grey,
             borderRadius: BorderRadius.circular(10.0)),
         child: Stack(
           children: [
             assetsLoaded && currentBtnId == widget.buttonId
-                ? SpriteWidget(rootNode)
+                ? ClipRect(child: SpriteWidget(rootNode))
                 : Container(),
           ],
         ),
@@ -131,18 +145,25 @@ class _AnimatedBGButtonState extends State<AnimatedBGButton> {
 
 // 動畫播放的區域
 class ANArea extends NodeWithSize {
-  int btnId;
+  List<LightSpot> lightSpotList = [];
+  List<FloatDiamond> floatDiamondList = [];
+  List<DiamondSprite> diamondSpriteList = [];
 
-  ANArea({@required this.btnId}) : super(new Size(12.0, 6.0)) {
+  ANArea() : super(new Size(12.0, 6.0)) {
     _generateFlashDiamond();
     _generateFloatDiamond();
     _generateLightSpot();
+
+    _runDiamondSpriteAnimate();
+    _runLightSpotAnimate();
+    _runFloatDiamondAnimate();
   }
 
   void _generateLightSpot() {
     for (int i = 0; i < 3; i++) {
       final sprite = new LightSpot(order: i);
       this.addChild(sprite);
+      lightSpotList.add(sprite);
     }
   }
 
@@ -151,6 +172,7 @@ class ANArea extends NodeWithSize {
     for (int i = 0; i < 4; i++) {
       final sprite = new FloatDiamond(order: i);
       this.addChild(sprite);
+      floatDiamondList.add(sprite);
     }
   }
 
@@ -165,8 +187,70 @@ class ANArea extends NodeWithSize {
         final double yOffset = (1 + (i * 2)).toDouble();
         sprite.position = Offset(xOffset, yOffset);
         this.addChild(sprite);
+        diamondSpriteList.add(sprite);
         order++;
       }
+    }
+  }
+
+  set active(bool active) {
+    motions.stopAll();
+    _runDiamondSpriteAnimate();
+    _runLightSpotAnimate();
+    _runFloatDiamondAnimate();
+  }
+
+  void _runLightSpotAnimate() {
+    for (LightSpot lightSpot in lightSpotList) {
+      motions.run(
+        new MotionRepeatForever(new MotionSequence(<Motion>[
+          new MotionTween<double>(
+              (a) => lightSpot.opacity = a, 0, 0, lightSpot.delayTime),
+          new MotionTween<double>((a) => lightSpot.opacity = a, 0, 1, 1),
+          new MotionTween<double>((a) => lightSpot.opacity = a, 1, 0, 2),
+        ])),
+      );
+    }
+  }
+
+  void _runDiamondSpriteAnimate() {
+    // 要做動畫的格子
+    const activeIndex = [2, 11, 12, 15];
+
+    for (int index in activeIndex) {
+      motions.run(
+        new MotionRepeatForever(new MotionSequence(<Motion>[
+          // 當作延遲時間
+          new MotionTween<double>((a) => diamondSpriteList[index].opacity = a,
+              1, 1, diamondSpriteList[index].delayTime),
+          // 這樣可以漸變顏色
+          new MotionTween<Color>(
+              (a) => diamondSpriteList[index].colorOverlay = a,
+              Colors.white12,
+              Colors.white70,
+              1.0),
+          new MotionTween<Color>(
+              (a) => diamondSpriteList[index].colorOverlay = a,
+              Colors.white70,
+              Colors.white12,
+              1.0),
+        ])),
+      );
+    }
+  }
+
+  void _runFloatDiamondAnimate() {
+    for (FloatDiamond floatDiamond in floatDiamondList) {
+      motions.run(
+        new MotionRepeatForever(new MotionGroup([
+          new MotionTween<double>((a) => floatDiamond.opacity = a, 1, 0.3, 5),
+          new MotionTween<Offset>(
+              (a) => floatDiamond.position = a,
+              Offset(floatDiamond.xOffset, floatDiamond.yOffset),
+              Offset(floatDiamond.xOffset, 0.0),
+              5),
+        ])),
+      );
     }
   }
 }
@@ -179,8 +263,7 @@ class DiamondSprite extends Sprite {
   //  12, 13, 14, 15, 16, 17
 
   final int order; // 編號
-  int delayTime;
-  bool needAnimate = false;
+  double delayTime;
   DiamondSprite({@required this.order})
       : super.fromImage(_images['assets/diamond.png']) {
     // Size(2,2) 因為我整個畫布是 寬 12,高 6, 這樣可以塞入共18個菱形
@@ -188,47 +271,40 @@ class DiamondSprite extends Sprite {
 
     switch (order) {
       case 2:
-        needAnimate = true;
-        delayTime = 1;
+        delayTime = 0;
         break;
       case 11:
-        needAnimate = true;
-        delayTime = 1;
+        delayTime = 0;
         break;
       case 12:
-        needAnimate = true;
-        delayTime = 4;
+        delayTime = 2;
         break;
       case 15:
-        needAnimate = true;
         delayTime = 3;
         break;
     }
-    if (needAnimate) {
-      _playAnimation(delayTime: delayTime);
-    }
   }
 
-  //播放動畫
-  void _playAnimation({@required int delayTime}) {
-    Timer(Duration(seconds: delayTime), () {
-      _flashAnimation();
-    });
-  }
-
-  // 閃爍動畫
-  void _flashAnimation() {
-    // 閃爍動畫
-    motions.run(
-      new MotionRepeatForever(new MotionSequence(<Motion>[
-        // 這樣可以漸變顏色
-        new MotionTween<Color>(
-            (a) => this.colorOverlay = a, Colors.white12, Colors.white70, 2.0),
-        new MotionTween<Color>(
-            (a) => this.colorOverlay = a, Colors.white70, Colors.white12, 2.0),
-      ])),
-    );
-  }
+//  //播放動畫
+//  void _playAnimation({@required int delayTime}) {
+//    Timer(Duration(seconds: delayTime), () {
+//      _flashAnimation();
+//    });
+//  }
+//
+//  // 閃爍動畫
+//  void _flashAnimation() {
+//    // 閃爍動畫
+//    motions.run(
+//      new MotionRepeatForever(new MotionSequence(<Motion>[
+//        // 這樣可以漸變顏色
+//        new MotionTween<Color>(
+//            (a) => this.colorOverlay = a, Colors.white12, Colors.white70, 2.0),
+//        new MotionTween<Color>(
+//            (a) => this.colorOverlay = a, Colors.white70, Colors.white12, 2.0),
+//      ])),
+//    );
+//  }
 }
 
 // 往上飄動的菱形
@@ -273,38 +349,39 @@ class FloatDiamond extends Sprite {
     this.position = Offset(xOffset, yOffset);
     this.colorOverlay = Colors.white54;
 
-    animation();
+//    animation();
   }
 
-  void animation() {
-    motions.run(
-      new MotionRepeatForever(new MotionGroup([
-        new MotionTween<double>((a) => opacity = a, 1, 0.3, 5),
-        new MotionTween<Offset>((a) => position = a, Offset(xOffset, yOffset),
-            Offset(xOffset, 0.0), 5),
-      ])),
-    );
-  }
+//  void animation() {
+//    motions.run(
+//      new MotionRepeatForever(new MotionGroup([
+//        new MotionTween<double>((a) => opacity = a, 1, 0.3, 5),
+//        new MotionTween<Offset>((a) => position = a, Offset(xOffset, yOffset),
+//            Offset(xOffset, 0.0), 5),
+//      ])),
+//    );
+//  }
 }
 
 class LightSpot extends Sprite {
   final int order; // 編號
   Offset spritePosition;
-  int delayTime;
+//  int delayTime;
+  double delayTime;
   LightSpot({@required this.order})
       : super.fromImage(_images['assets/light_spot.png']) {
     switch (order) {
       case 0:
         spritePosition = Offset(3, 2);
-        delayTime = 1;
+        delayTime = 0;
         break;
       case 1:
         spritePosition = Offset(3, 4);
-        delayTime = 3;
+        delayTime = 2;
         break;
       case 2:
         spritePosition = Offset(9, 4);
-        delayTime = 5;
+        delayTime = 1;
         break;
     }
 
@@ -313,24 +390,24 @@ class LightSpot extends Sprite {
     this.rotation = 63;
     opacity = 0;
 
-    _playAnimation(delayTime: delayTime);
+//    _playAnimation(delayTime: delayTime);
   }
 
-  //播放動畫
-  void _playAnimation({@required int delayTime}) {
-    Timer(Duration(seconds: delayTime), () {
-      _flashAnimation();
-    });
-  }
-
-  // 閃爍動畫
-  void _flashAnimation() {
-    // 閃爍動畫
-    motions.run(
-      new MotionRepeatForever(new MotionSequence(<Motion>[
-        new MotionTween<double>((a) => opacity = a, 0, 1, 1),
-        new MotionTween<double>((a) => opacity = a, 1, 0, 2),
-      ])),
-    );
-  }
+//  //播放動畫
+//  void _playAnimation({@required int delayTime}) {
+//    Timer(Duration(seconds: delayTime), () {
+//      _flashAnimation();
+//    });
+//  }
+//
+//  // 閃爍動畫
+//  void _flashAnimation() {
+//    // 閃爍動畫
+//    motions.run(
+//      new MotionRepeatForever(new MotionSequence(<Motion>[
+//        new MotionTween<double>((a) => opacity = a, 0, 1, 1),
+//        new MotionTween<double>((a) => opacity = a, 1, 0, 2),
+//      ])),
+//    );
+//  }
 }
