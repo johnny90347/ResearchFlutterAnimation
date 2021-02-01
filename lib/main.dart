e543458bb903e3c6b6d856748e6d6b4bdaace4e9
johnny <>
2021-02-01 15:51:23 +0800

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,13 @@ NodeWithSize _anArea;
 
 ImageMap _images;
 
-class FirstPage extends StatelessWidget {
+int currentBtnId = 0;
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,9 +39,15 @@ class FirstPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedBGButton(onPressed: null),
-            AnimatedBGButton(onPressed: null),
-            AnimatedBGButton(onPressed: null)
+            AnimatedBGButton(buttonId:0,onPressed: (){
+              currentBtnId = 0;
+            }),
+            AnimatedBGButton(buttonId:1,onPressed: (){
+              currentBtnId = 1;
+            }),
+            AnimatedBGButton(buttonId:2,onPressed: (){
+              currentBtnId = 2;
+            })
           ],
         ),
       ),
@@ -46,7 +58,8 @@ class FirstPage extends StatelessWidget {
 // 動畫按鈕
 class AnimatedBGButton extends StatefulWidget {
   final VoidCallback onPressed;
-  AnimatedBGButton({@required this.onPressed});
+  final int buttonId;
+  AnimatedBGButton({@required this.buttonId,@required this.onPressed});
 
   @override
   _AnimatedBGButtonState createState() => _AnimatedBGButtonState();
@@ -61,7 +74,7 @@ class _AnimatedBGButtonState extends State<AnimatedBGButton> {
     // 使用load 方法 (非同步) 去取得圖片
     await _images.load(<String>[
       'assets/diamond.png',
-      'assets/light.png',
+      'assets/light_spot.png',
     ]);
   }
 
@@ -75,7 +88,7 @@ class _AnimatedBGButtonState extends State<AnimatedBGButton> {
       (value) {
         setState(() {
           assetsLoaded = true;
-          _anArea = new ANArea();
+          _anArea = new ANArea(btnId: widget.buttonId);
         });
       },
     );
@@ -104,15 +117,30 @@ class _AnimatedBGButtonState extends State<AnimatedBGButton> {
 
 // 動畫播放的區域
 class ANArea extends NodeWithSize {
-  ANArea() : super(new Size(12.0, 6.0)) {
+  int btnId;
 
+  List<DiamondSprite> diamondSpriteList = [];
+
+  ANArea({@required this.btnId}) : super(new Size(12.0, 6.0)) {
     _generateFlashDiamond();
+//    _generateFloatDiamond();
+//    _generateLightSpot();
 
-    for (int i=0;i<4 ;i++){
+  }
+
+  void _generateLightSpot(){
+    for (int i = 0; i < 3; i++) {
+      final sprite = new LightSpot(order: i);
+      this.addChild(sprite);
+    }
+  }
+
+  // 往上飄的diamon
+  void _generateFloatDiamond() {
+    for (int i = 0; i < 4; i++) {
       final sprite = new FloatDiamond(order: i);
       this.addChild(sprite);
     }
-
   }
 
   // 產生動畫菱形
@@ -126,9 +154,44 @@ class ANArea extends NodeWithSize {
         final double yOffset = (1 + (i * 2)).toDouble();
         sprite.position = Offset(xOffset, yOffset);
         this.addChild(sprite);
+       diamondSpriteList.add(sprite);
         order++;
       }
     }
+
+
+
+  }
+
+
+  set active(bool active) {
+    motions.stopAll();
+    if(active){
+      _playAnimation(sprite: diamondSpriteList[2], delayTime: 1);
+    }
+  }
+
+
+
+  //播放動畫
+  void _playAnimation({@required Sprite sprite,@required int delayTime}) {
+    Timer(Duration(seconds: delayTime), () {
+      _flashAnimation(sprite: sprite);
+    });
+  }
+
+  // 閃爍動畫
+  void _flashAnimation({@required Sprite sprite}) {
+    // 閃爍動畫
+    motions.run(
+      new MotionRepeatForever(new MotionSequence(<Motion>[
+        // 這樣可以漸變顏色
+        new MotionTween<Color>(
+                (a) => sprite.colorOverlay = a, Colors.white12, Colors.white70, 2.0),
+        new MotionTween<Color>(
+                (a) => sprite.colorOverlay = a, Colors.white70, Colors.white12, 2.0),
+      ])),
+    );
   }
 }
 
@@ -140,27 +203,37 @@ class DiamondSprite extends Sprite {
   //  12, 13, 14, 15, 16, 17
 
   final int order; // 編號
-
+  int delayTime;
+  bool needAnimate = false;
   DiamondSprite({@required this.order})
       : super.fromImage(_images['assets/diamond.png']) {
     // Size(2,2) 因為我整個畫布是 寬 12,高 6, 這樣可以塞入共18個菱形
     this.size = Size(2, 2);
 
+
     switch (order) {
       case 2:
-        playAnimation(delayTime: 1);
+        needAnimate = true;
+        delayTime = 1;
         break;
       case 11:
-        playAnimation(delayTime: 1);
+        needAnimate = true;
+        delayTime = 1;
         break;
       case 12:
-        playAnimation(delayTime: 4);
+        needAnimate = true;
+        delayTime = 4;
         break;
       case 15:
-        playAnimation(delayTime: 3);
+        needAnimate = true;
+        delayTime = 3;
         break;
     }
+
+//    _playAnimation(delayTime: delayTime);
   }
+
+
 
 //  void doBGAnimation(Timer timer) {
 //
@@ -179,14 +252,14 @@ class DiamondSprite extends Sprite {
 //  }
 
   //播放動畫
-  void playAnimation({@required int delayTime}) {
+  void _playAnimation({@required int delayTime}) {
     Timer(Duration(seconds: delayTime), () {
-      flashAnimation();
+      _flashAnimation();
     });
   }
 
   // 閃爍動畫
-  void flashAnimation() {
+  void _flashAnimation() {
     // 閃爍動畫
     motions.run(
       new MotionRepeatForever(new MotionSequence(<Motion>[
@@ -204,41 +277,38 @@ class DiamondSprite extends Sprite {
 class FloatDiamond extends Sprite {
   final int order; // 編號
 
- double xOffset;
- double yOffset;
+  double xOffset;
+  double yOffset;
   double scaleValue;
   Color colorCover;
-  FloatDiamond({@required this.order}) : super.fromImage(_images['assets/diamond.png']) {
-
+  FloatDiamond({@required this.order})
+      : super.fromImage(_images['assets/diamond.png']) {
     switch (order) {
       case 0:
         xOffset = 1.5;
-        yOffset =5;
+        yOffset = 5;
         scaleValue = 1.3;
         colorCover = Colors.white54;
         break;
       case 1:
-        xOffset =4;
-        yOffset =4;
+        xOffset = 4;
+        yOffset = 4;
         scaleValue = 1.0;
         colorCover = Colors.white54;
         break;
       case 2:
         xOffset = 8.5;
-        yOffset =4.5;
+        yOffset = 4.5;
         scaleValue = 1.5;
         colorCover = Colors.white24;
         break;
       case 3:
         xOffset = 11;
-        yOffset =5.5;
+        yOffset = 5.5;
         scaleValue = 1.1;
         colorCover = Colors.white54;
         break;
     }
-
-
-
 
     this.size = Size(1, 1);
     this.scale = scaleValue;
@@ -251,9 +321,59 @@ class FloatDiamond extends Sprite {
   void animation() {
     motions.run(
       new MotionRepeatForever(new MotionGroup([
-        new MotionTween<double>((a) => opacity = a, 1, 0, 13),
-        new MotionTween<Offset>(
-            (a) => position = a,Offset(xOffset, yOffset), Offset(xOffset, 0.0),13),
+        new MotionTween<double>((a) => opacity = a, 1, 0.3, 13),
+        new MotionTween<Offset>((a) => position = a, Offset(xOffset, yOffset),
+            Offset(xOffset, 0.0), 13),
+      ])),
+    );
+  }
+}
+
+class LightSpot extends Sprite {
+  final int order; // 編號
+  Offset spritePosition;
+  int delayTime;
+  LightSpot({@required this.order})
+      : super.fromImage(_images['assets/light_spot.png']) {
+
+
+    switch (order) {
+      case 0:
+        spritePosition = Offset(3, 2);
+        delayTime = 1;
+        break;
+      case 1:
+        spritePosition = Offset(3, 4);
+        delayTime = 3;
+        break;
+      case 2:
+        spritePosition = Offset(9, 4);
+        delayTime = 5;
+        break;
+    }
+
+    this.size = Size(2, 2);
+    this.position = spritePosition;
+    this.rotation = 63;
+    opacity = 0;
+
+    _playAnimation(delayTime: delayTime);
+  }
+
+  //播放動畫
+  void _playAnimation({@required int delayTime}) {
+    Timer(Duration(seconds: delayTime), () {
+      _flashAnimation();
+    });
+  }
+
+  // 閃爍動畫
+  void _flashAnimation() {
+    // 閃爍動畫
+    motions.run(
+      new MotionRepeatForever(new MotionSequence(<Motion>[
+        new MotionTween<double>((a) => opacity = a, 0, 1, 2),
+        new MotionTween<double>((a) => opacity = a, 1, 0, 2),
       ])),
     );
   }
